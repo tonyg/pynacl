815b2afb39d71394d5aeb4d9a4d3361bc5a54279
Brian Warner <warner@lothar.com>
2012-01-23 15:15:28 -0800

    benchmark tools for the pynacl bindings

---
+++ compare.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+
+import sys, json
+
+def abbreviate_small_time(s, spacer="", suffix="s"):
+    def _unit(text, unit):
+        return text+spacer+unit+suffix
+    if s > 1:
+        return _unit("%d"%s, "")
+    if s > 1e-3:
+        return _unit("%0.2f" % (s*1e3), "m")
+    if s > 1e-6:
+        return _unit("%0.2f" % (s*1e6), "u")
+    if s > 1e-9:
+        return _unit("%0.2f" % (s*1e9), "n")
+    return _unit("%g" % s, "")
+
+portable = json.load(open(sys.argv[1]))
+optimized = json.load(open(sys.argv[2]))
+
+print "%-30s %10s%10s%10s" % ("", "portable", "optimized", "slowdown")
+for k in sorted(portable):
+    slowdown = "%.1fx" % (portable[k] / optimized[k])
+    print "%-30s:%10s%10s%10s" % (k,
+                                  abbreviate_small_time(portable[k]),
+                                  abbreviate_small_time(optimized[k]),
+                                  slowdown)
+
+f = open("compare.html", "w")
+f.write("""<html><body>
+<style>
+table {border: 1px solid #CCC;}
+td,th {border: 1px solid #CCC;}
+td.name {text-align: left; }
+td.data {text-align: right; }
+th {padding-left: 10px; padding-right: 10px; }
+
+</style>
+<table cellspacing=0>
+<tr><th>name</th><th>ref</th><th>optimized</th><th>slowdown</th></tr>
+""")
+for k in sorted(portable):
+    slowdown = portable[k] / optimized[k]
+    f.write('<tr>')
+    f.write('<td class="name">%s</td>' % k)
+    f.write('<td class="data">%s</td>' % abbreviate_small_time(portable[k],
+                                                               spacer=" "))
+    f.write('<td class="data">%s</td>' % abbreviate_small_time(optimized[k],
+                                                               spacer=" "))
+    if slowdown < 2:
+        color = "#AFA"
+    elif slowdown < 4:
+        color = "#FFA"
+    else:
+        color = "#FAA"
+    f.write('<td class="data" style="background-color: %s">%.1fx</td>' %
+            (color, slowdown))
+    f.write('</tr>\n')
+f.write("""
+</table>
+</body>
+</html>
+""")
+f.close()
+
+f = open("compare2.html", "w")
+f.write("""<html><body>
+<style>
+th {padding-left: 10px; padding-right: 10px; }
+
+</style>
+<table cellspacing=0 style="border: 1px solid #CCC;">
+<tr><th style="padding-left: 10px; padding-right: 10px;">name</th><th style="padding-left: 10px; padding-right: 10px;">ref</th><th style="padding-left: 10px; padding-right: 10px;">optimized</th><th style="padding-left: 10px; padding-right: 10px;">slowdown</th></tr>
+""")
+for k in sorted(portable):
+    slowdown = portable[k] / optimized[k]
+    f.write('<tr>')
+    f.write('<td style="border: 1px solid #CCC; text-align: left">%s</td>' % k)
+    f.write('<td style="border: 1px solid #CCC; text-align: right">%s</td>' % abbreviate_small_time(portable[k],
+                                                               spacer=" "))
+    f.write('<td style="border: 1px solid #CCC; text-align: right">%s</td>' % abbreviate_small_time(optimized[k],
+                                                               spacer=" "))
+    if slowdown < 2:
+        color = "#AFA"
+    elif slowdown < 4:
+        color = "#FFA"
+    else:
+        color = "#FAA"
+    f.write('<td style="border: 1px solid #CCC; text-align: right; background-color: %s">%.1fx</td>' %
+            (color, slowdown))
+    f.write('</tr>\n')
+f.write("""
+</table>
+</body>
+</html>
+""")
+f.close()
+
+
---
+++ speed.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import sys, os, time, json
+from distutils import util
+
+libdir = os.path.join("build",
+                      "lib.%s-%s" % (util.get_platform(), sys.version[:3]))
+sys.path.insert(0, libdir)
+
+import nacl
+
+def timeit(f, *args):
+    count = 1
+    while True:
+        #print "trying", count
+        start = time.time()
+        for i in xrange(count):
+            f(*args)
+        end = time.time()
+        elapsed = end - start
+        each = elapsed / count
+        #print " took %g, each=%g" % (elapsed, each)
+        if count > 3 and elapsed > 1.0:
+            return each, count, elapsed
+        count *= 2
+
+data = {}
+def do(name, *args):
+    each, count, elapsed = timeit(getattr(nacl, name), *args)
+    print "%s: %g" % (name, each)
+    data[name] = each
+
+do("crypto_hash_sha256", b"")
+do("crypto_hash_sha512", b"")
+
+scalar0 = b"0"*32
+scalar1 = b"1"*32
+do("crypto_scalarmult", scalar0, scalar1)
+do("crypto_scalarmult_base", scalar0)
+
+msg = b"m"*1000
+nonce = b"n"*nacl.crypto_stream_NONCEBYTES
+key = b"k"*nacl.crypto_stream_KEYBYTES
+do("crypto_stream", 1000, nonce, key)
+do("crypto_stream_xor", msg, nonce, key)
+
+msg = b"m"*1000
+key = b"k"*nacl.crypto_auth_KEYBYTES
+do("crypto_auth", msg, key)
+a = nacl.crypto_auth(msg, key)
+do("crypto_auth_verify", a, msg, key)
+
+do("crypto_onetimeauth", msg, key)
+a = nacl.crypto_onetimeauth(msg, key)
+do("crypto_onetimeauth_verify", a, msg, key)
+
+msg = b"m"*1000
+nonce = b"n"*nacl.crypto_stream_NONCEBYTES
+key = b"k"*nacl.crypto_auth_KEYBYTES
+do("crypto_secretbox", msg, nonce, key)
+c = nacl.crypto_secretbox(msg, nonce, key)
+do("crypto_secretbox_open", c, nonce, key)
+
+do("crypto_box_keypair")
+pk1, sk1 = nacl.crypto_box_keypair()
+pk2, sk2 = nacl.crypto_box_keypair()
+do("crypto_box", msg, nonce, pk1, sk2)
+c = nacl.crypto_box(msg, nonce, pk1, sk2)
+do("crypto_box_open", c, nonce, pk2, sk1)
+
+do("crypto_sign_keypair_fromseed", b"seed")
+pk,sk = nacl.crypto_sign_keypair()
+do("crypto_sign", msg, sk)
+sm = nacl.crypto_sign(msg, sk)
+do("crypto_sign_open", sm, pk)
+
+open("speed.json", "w").write(json.dumps(data))
